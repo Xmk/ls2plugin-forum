c0dd90623e313788b3003f9baf9c260c2b1cc152
Chiffa <i@ars-team.ru>
2016-12-09 02:17:09 +0500

    Базовая структура плагина

---
+++ PluginForum.class.php
@@ -0,0 +1,36 @@
+<?php
+
+if (!class_exists('Plugin')) {
+    die('Hacking attemp!')
+}
+
+class PluginForum extends Plugin
+{
+    protected $aInherits = array(
+
+    );
+
+    /**
+     * Инициализация плагина
+     */
+    public function Init()
+    {
+
+    }
+
+    /**
+     * Активация плагина
+     */
+    public function Activate()
+    {
+
+    }
+
+    /**
+     * Деактивация плагина
+     */
+    public function Deactivate()
+    {
+
+    }
+}
